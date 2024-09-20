776cf829f1d9280a6942aad5e44b473988ee36b5
Saif <saifisami2005@gmail.com>
2024-09-19 23:19:25 -0400

    Implementederror handling and made the code even cleaner.

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const express = require("express");
-const Statistic = require("./helpers")
+const express = require("express"), Statistic = require("./helpers"), app = express();
 
-const app = express();
+let theErrorValue;
 
 const createResponse = (operation, value) => {
     const anArr = [];
     value.split(",").forEach(num => anArr.push(Number(num)));
+    for (let num of value.split(",")) {
+        if (isNaN(Number(num))) {
+            theErrorValue = num;
+            throw new Error
+        }
+    }
     return { response: { operation, "value": Statistic[operation](anArr) } }
 }
 
-app.get("/mean", (req, res) => res.json(createResponse("mean", req.query["nums"])))
+for (let operation of ["mean", "median", "mode"]) app.get(`/${operation}`, (req, res) => res.json(createResponse(operation, req.query["nums"])));
 
-app.get("/median", (req, res) => res.json(createResponse("median", req.query["nums"])))
+app.use((error, req, res, next) => {
+    res.status(400);
+    if (req.query["nums"]) return res.json({ response: `${theErrorValue} is not a number.` });
+    else return res.send("Incorrect query - nums are required.");
+})
 
-app.get("/mode", (req, res) => res.json(createResponse("mode", req.query["nums"])))
-
-app.listen(3000, () => console.log("App on port 3000."))
+app.listen(3000, () => console.log("App on port 3000."));
